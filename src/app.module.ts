e54cbabd4cc8ba6253b1f0ae7d349c7a2bef333d
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-11-17 09:27:11 -0300

    feat: create users resource and user entity, need install the validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './config/typeorm-config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
