3bc8cc466cc6b505961fea81a3f4c36f4396acc8
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-11-21 04:11:33 -0300

    feat: remove commented Redis configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,8 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './commom/common.module';
 import { DatabaseModule } from './database/database.module';
-// import { RedisModule } from 'nestjs-redis';
-// import { Logger } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { AppCacheModule } from './cache/cache.module';
-
-// RedisModule.register({
-//   host: 'redis',
-//   port: 6379,
-//   onClientReady(client) {
-//     client.on('error', (err) => {
-//       console.error(err);
-//     });
-//     new Logger.log('Redis client is ready');
-//   },
-// }),
 
 @Module({
   imports: [
